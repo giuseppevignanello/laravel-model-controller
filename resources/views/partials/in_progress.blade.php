94ce9d1c949ed3af503eebc8b423437379f82ace
giuseppevignanello <peppe.vignanello@gmail.com>
2023-05-24 16:31:51 +0200

    add in progress partial

---
+++ resources/views/partials/in_progress.blade.php
@@ -0,0 +1,10 @@
+<div class="about d-flex align-items-center justify-content-center py-5">
+    <div class="work_in_progress_image">
+        <img src="{{ Vite::asset('resources/img/work-in-progress.svg') }}" alt="">
+    </div>
+    <h1 class="text-white">This section is in progress</h1>
+    <div class="work_in_progress_image">
+        <img src="{{ Vite::asset('resources/img/work-in-progress.svg') }}" alt="">
+    </div>
+
+</div>
