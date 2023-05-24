57af1b669f25ff965c9dcfed0ab4ae926accdeea
giuseppevignanello <peppe.vignanello@gmail.com>
2023-05-24 15:42:15 +0200

    add movies data dynamically

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Prova</h1>
+    @forelse ($movies as $movie)
+        <h3>{{ $movie->title }}</h3>
+        <h5>{{ $movie->original_title }}</h5>
+        <h6>{{ $movie->nationality }}</h6>
+    @empty
+    @endforelse
 @endsection
