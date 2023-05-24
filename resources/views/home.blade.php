0e571a599200a7f8a0fc22ee7b80e2196eb6b2e9
giuseppevignanello <peppe.vignanello@gmail.com>
2023-05-24 16:18:35 +0200

    fix movies images

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
         <div class="row justify-content-center justify-content-md-between">
             @forelse ($movies as $movie)
                 <div class="card  my-3" style="width:18rem;">
-                    <img src="https://picsum.photos/200/300" class="card-img-top" alt="...">
+                    <img src="{{ $movie->image }}" class="card-img-top" alt="...">
                     <div class="card-body">
                         <h5 class="card-title">{{ $movie->title }}</h5>
                         <h6 class="card-subtitle mb-2 text-muted ">{{ $movie->original_title }}</h6>
